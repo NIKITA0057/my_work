df4c73c3f253d21b1922d36b04338fbd8e8a6655
nikita <vazaxac@gmail.com>
2024-01-07 10:52:29 +0300

    home_work_2

--- upload_to_disk.py
+++ upload_to_disk.py
@@ -6,71 +6,61 @@ import password
 from loguru import logger
 
 
-
 def get_file_local():
     file_list = os.listdir(password.PATH)
     return file_list
 
 
 def get_file_yadisk():
-    l1 = []
+    files_of_yandex = []
     client = yadisk.Client(token=password.TOKEN)
     disk_list = client.listdir('/main')
-    for i in disk_list:
-        l1.append(i.name)
-    return l1
+    for ya_file in disk_list:
+        files_of_yandex.append(ya_file.name)
+    return files_of_yandex
 
 
-
-
-
-def remove_file(l1, l2):
+def remove_file(files_list_yandex, file_list_local):
     client = yadisk.Client(token=password.TOKEN)
-
-    for file1 in l1:
-
-        for file2 in l2:
-            if file1 == file2:
-                continue
-            else:
-                logger.info(f'deleated file {file1}')
+    for file1 in files_list_yandex:
+        if file1 not in set(file_list_local):
+            try:
                 client.remove(f'/main/{file1}')
+                logger.info(f'file {file1} delited')
+            except Exception as e:
+                logger.error(f'file breake {file1}')
 
 
-def upload_to_yadisk(l1, l2):
+def upload_to_yadisk(files_list_yandex, file_list_local):
     social = yadisk.Client(token=password.TOKEN)
+    for file in file_list_local:
+        if os.path.basename(file) not in set(files_list_yandex):
+            try:
+                social.upload(password.PATH + f'/{file}', f'/main/{file}')
+                logger.info(f'file upload {file}')
+            except Exception as e:
+                logger.error(f'file don"t upload {file}')
 
-    for file2 in l2:
-        if len(l1) != 0:
-            for file1 in l1:
-                if file2 == file1:
-                    continue
-                else:
-                    social.upload(password.PATH + f'/{file2}', f'/main/{file2}')
-                    logger.info(f'upload file {file2}')
-        else:
-            social.upload(password.PATH + f'/{file2}', f'/main/{file2}')
-            logger.info(f'upload file {file2}')
 
-def update(l2):
+def update(file_list_local):
     social = yadisk.Client(token=password.TOKEN)
-    for update_file in l2:
-        social.upload(password.PATH + f'/{update_file}', f'/main/{update_file}',overwrite=True)
+    for update_file in file_list_local:
+        social.upload(password.PATH + f'/{update_file}', f'/main/{update_file}', overwrite=True)
     logger.info(f'update file')
-
 
 
 # l1 - yadisk
 # l2 = localdisk
 def main():
-    l1 = get_file_yadisk()
-    l2 = sorted(get_file_local())
-    if len(l1) > len(l2):
-        remove_file(l1, l2)
-    elif len(l2) > len(l1):
-        upload_to_yadisk(l1, l2)
+    files_list_yandex = get_file_yadisk()
+    file_list_local = sorted(get_file_local())
+    if len(files_list_yandex) > len(file_list_local):
+        remove_file(files_list_yandex, file_list_local)
+    elif len(file_list_local) > len(files_list_yandex):
+        upload_to_yadisk(files_list_yandex, file_list_local)
     else:
-        update(l2)
+        update(file_list_local)
+
 
 if __name__ == '__main__':
     while True:
